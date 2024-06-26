0f2b3d4846320b16335f34da18b8b810cdc81f73
TUSHAR SINGH <161207026+TuShhar0007@users.noreply.github.com>
2024-06-26 10:48:17 +0530

    Add files via upload

---
+++ itertools.product_prob_hackerrank.py
@@ -0,0 +1,13 @@
+
+''' itertools.product()  probelem hackerrank # solved'''
+
+# from itertools import product
+# l = input()
+# k = input()
+# b = l.split()
+# d = k.split()
+# c = [int(num) for num in b]
+# e = [int(second_list) for second_list in d]
+# f  = tuple(product(c,e))
+# for item in f:
+#     print(item, end=' ')
