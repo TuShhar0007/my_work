ee9eb0c1ab99657b9223927dfb099ad3bcc70787
TUSHAR SINGH <161207026+TuShhar0007@users.noreply.github.com>
2024-06-26 10:49:30 +0530

    Add files via upload

---
+++ itertool.permutations_prob_hackerrank.py
@@ -0,0 +1,13 @@
+''' itertool.permutations() '''
+'''solved'''
+# from itertools import permutations
+# s = input()
+# c = []
+# for i in s:
+#     if i.isalpha()== True:
+#         c.append(i)
+#     else:
+#         D = i
+# c = sorted(list(permutations(c,int(D))))
+# for words in c:
+#     print("".join(words))
