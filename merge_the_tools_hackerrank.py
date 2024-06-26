972650e78631d2a0d054c06dec3457627a3b5bf7
TUSHAR SINGH <161207026+TuShhar0007@users.noreply.github.com>
2024-06-26 10:51:35 +0530

    Add files via upload

---
+++ merge_the_tools_hackerrank.py
@@ -0,0 +1,8 @@
+# import textwrap
+# def merge_the_tools(string, k):
+#     t = textwrap.wrap(string, k)
+#     for i in t:
+#         print(dict.fromkeys(i))
+#         print(''.join(dict.fromkeys(i)))
+# string, k = input(), int(input())
+# merge_the_tools(string, k)
