4e627ef2d624300a57b047b4dabf946cbc078c57
TUSHAR SINGH <161207026+TuShhar0007@users.noreply.github.com>
2024-06-27 14:07:03 +0530

    ginortsprob_hackerrank.py

--- ginortsprob_hackerrank.py
+++ ginortsprob_hackerrank.py
@@ -4,25 +4,25 @@ bc kaise aakhir kaar kaise"""
 # ginorts problem qeustion hacker rank
 
 
-# s = input()
-# c = list(s)
-# d = []
-# l = [] 
-# n = []
-# t = []
-# e= []
-# for i in c:
-#     if i.islower()== True:
-#         l.append(i)
-# for a in c:
-#        if a.isupper()==True:
-#            n.append(a)
-# for b in c:
-#     if b.isnumeric()==True:
-#         if int(b)%2!=0:
-#             t.append(b)
-# m = [e.append(h) for h in c if h not in t and h.isnumeric()==True]
-# w = (sorted(l)+sorted(n)+sorted(t)+sorted(e))
-# print("".join(w))
+s = input()
+c = list(s)
+d = []
+l = [] 
+n = []
+t = []
+e= []
+for i in c:
+    if i.islower()== True:
+        l.append(i)
+for a in c:
+       if a.isupper()==True:
+           n.append(a)
+for b in c:
+    if b.isnumeric()==True:
+        if int(b)%2!=0:
+            t.append(b)
+m = [e.append(h) for h in c if h not in t and h.isnumeric()==True]
+w = (sorted(l)+sorted(n)+sorted(t)+sorted(e))
+print("".join(w))
 
 
